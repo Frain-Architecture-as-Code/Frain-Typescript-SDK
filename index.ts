aac376901990f1ba8ccdd1587e2bf27eb34a8921
JonatanFD <u20211b995@upc.edu.pe>
2026-01-25 13:06:52 -0500

    feat: prepared for publishment

--- index.ts
+++ index.ts
@@ -1,1 +1,13 @@
-console.log("Hello via Bun!");
+export { Frain } from "./src/frain";
+export {
+    Person,
+    System,
+    ExternalSystem,
+    Container,
+    Component,
+} from "./src/frain-nodes";
+export { ComponentView } from "./src/component-view";
+export { ContainerView } from "./src/container-view";
+export { ContextView } from "./src/context-view";
+export { View } from "./src/view";
+export * from "./src/types";
